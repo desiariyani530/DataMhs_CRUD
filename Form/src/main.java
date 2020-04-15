c184402f9f9a1b289a190b3c6630cc472837db2b
desiariyani530 <61138472+desiariyani530@users.noreply.github.com>
2020-04-15 13:09:50 +0700

    Add files via upload

---
+++ Form/src/main.java
@@ -0,0 +1,12 @@
+
+public class main {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+       formmhs fm = new formmhs();
+       fm.tesformmhs();
+      
+    }
+    
+}
